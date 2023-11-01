9a763c4e72f954079e2e03141c900c2c43f2ddfb
Likeur <jacksimba28@gmail.com>
2023-11-01 13:11:58 +0200

    initialisation of the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode : 'class',
+  content: [
+    './**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
