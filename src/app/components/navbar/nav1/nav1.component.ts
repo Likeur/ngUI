fa26f5e555cb43d4004e007584ba094ff8b5ffcf
Likeur <jacksimba28@gmail.com>
2023-11-01 19:10:45 +0200

    add navbar component

---
+++ src/app/components/navbar/nav1/nav1.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav1.component.html',
+  styles: [
+  ]
+})
+export class Nav1Component {
+
+  isOpenNav(){
+    function openNav(){
+      const navBar = document.querySelector('#nav-bar')!
+      const barUn = document.querySelector('#barUn')!
+      const barDeux = document.querySelector('#barDeux')!
+
+      navBar.classList.toggle('translate-x-[1000px]')
+      barUn.classList.toggle('rotate-45')
+      barDeux.classList.toggle('rotate-45')
+
+    }
+    openNav()
+  }
+}
