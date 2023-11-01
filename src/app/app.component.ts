51930789c3e24263d9cc9dfbf4acac95d290bbe6
Likeur <jacksimba28@gmail.com>
2023-11-01 19:11:35 +0200

    add navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Footer1Component } from './components/footer/footer1/footer1.component';
+import { Nav1Component } from './components/navbar/nav1/nav1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Footer1Component],
+  imports: [CommonModule, RouterOutlet, Nav1Component, Footer1Component],
   templateUrl: './app.component.html',
   styles: [],
 })
