779a3215e7b93fdce84869a5d7706f0597e1a163
Likeur <jacksimba28@gmail.com>
2023-11-06 15:33:09 +0200

    feature section fixe

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Footer1Component } from 'src/app/components/footer/footer1/footer1.component';
 import { RouterModule } from '@angular/router';
+import { Featuresec1Component } from 'src/app/components/feature-section/featuresec1/featuresec1.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule,Footer1Component, RouterModule],
+  imports: [CommonModule,Footer1Component, RouterModule, Featuresec1Component],
   templateUrl: './landing.component.html',
   
 })
