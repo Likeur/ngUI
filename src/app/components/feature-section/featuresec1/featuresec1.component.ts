e40d0fda449f3cbe6e3eec535d7dec2dfd10ee75
Likeur <jacksimba28@gmail.com>
2023-11-06 15:24:18 +0200

    feature block

---
+++ src/app/components/feature-section/featuresec1/featuresec1.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'featuresec1',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './featuresec1.component.html',
+  styles: [
+  ]
+})
+export class Featuresec1Component {
+
+}
---
+++ src/app/components/feature-section/featuresec2/featuresec2.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-featuresec2',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './featuresec2.component.html',
+  styles: [
+  ]
+})
+export class Featuresec2Component {
+
+}
