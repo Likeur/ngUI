a0aa4edd78502fa33401b698c38a7c3d7a87f638
Likeur <jacksimba28@gmail.com>
2023-11-09 00:10:32 +0200

    hero section preview fixe

---
+++ src/app/pages/blocks/service-feature/service-feature.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-service-feature',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './service-feature.component.html',
+  styles: [
+  ]
+})
+export class ServiceFeatureComponent {
+
+}
